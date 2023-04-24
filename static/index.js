e048ac5fc9fc1ed980cb1c5b4dff8dc2fdca5f2a
nuker <rinards.sondors@gmail.com>
2023-04-24 15:15:11 +0100

    preact integration

--- static/index.js
+++
@@ -1,6 +0,0 @@
-// import { h, render } from "https://esm.sh/preact";
-// // Initialize htm with Preact
-// const app = h("h1", null, "Hello World");
-// render(app, document.body);
-
-alert("HELLO JS");
---
+++ static/index.mjs
@@ -0,0 +1,32 @@
+import { h, render } from "https://esm.sh/preact";
+import htm from "https://unpkg.com/htm?module";
+
+// Initialize htm with Preact
+const html = htm.bind(h);
+
+function App(props) {
+  return html`
+    <h1>LIVE CPU GO</h1>
+    <div>
+      ${props.cpus.map((cpu, i) => {
+        i++;
+        return html`
+          <div class="outer">
+            <div class="cores">CPU ${i}</div>
+            <div class="cpu">${cpu.toFixed(1)} %</div>
+          </div>
+        `;
+      })}
+    </div>
+  `;
+}
+
+let url = new URL("/realtime/cpu", window.location.href);
+
+url.protocol = url.protocol.replace("http", "ws");
+
+let ws = new WebSocket(url.href);
+ws.onmessage = (ev) => {
+  let json = JSON.parse(ev.data);
+  render(html`<${App} cpus=${json}></${App}>`, document.body);
+};
